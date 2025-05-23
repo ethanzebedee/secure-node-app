5db8b00e235eb06f34cdb3024ed14490db07d804
Ethan <gonedaft890@gmail.com>
2025-05-23 14:42:59 +0100

    Initial files created

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express"),
+  app = express(),
+  port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
